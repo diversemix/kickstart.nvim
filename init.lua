38348dd17b519f3f4c7148f332dad1a675d409ef
Peter Hooper <diversemix@gmail.com>
2025-10-04 18:41:28 +0100

    Remove 8-bit terminal color settings
    Removed terminal color settings for 8-bit foreground and background.

--- init.lua
+++ init.lua
@@ -152,8 +152,6 @@ vim.o.splitbelow = true
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.termguicolors = true
-vim.o.t_8f = "<Esc>[38;2;%lu;%lu;%lum"
-vim.o.t_8b = "<Esc>[48;2;%lu;%lu;%lum"
 
 -- Preview substitutions live, as you type!
 vim.o.inccommand = 'split'
