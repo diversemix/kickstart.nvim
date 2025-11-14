4ef033126713b74ad387bf16e138cec75e674178
diversemix <diversemix@gmail.com>
2025-11-14 16:06:10 +0000

    use silversearcher

--- init.lua
+++ init.lua
@@ -407,12 +407,23 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
+        defaults = {
+          vimgrep_arguments = {
+            'ag',
+            '--nocolor',
+            '--nogroup',
+            '--hidden',
+            '--vimgrep',
+            '--ignore-case'
+          },
+          find_command = { 'ag', '--nocolor', '--nogroup', '--hidden', '-g', '' },
+          mappings = {
+            i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          },
+        },
+        pickers = {
+            find_command = { 'ag', '--nocolor', '--nogroup', '--hidden', '-g', '' },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
