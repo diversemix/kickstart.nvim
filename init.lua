e21afd83ca59fea3c829658b454e57acd3175af3
diversemix <diversemix@gmail.com>
2025-11-30 16:35:18 +0000

    Add markdown preview

--- init.lua
+++ init.lua
@@ -317,6 +317,16 @@ require('lazy').setup({
     end,
   },
 
+  { -- Markdown preview in browser
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = 'cd app && npm install',
+    keys = {
+      { '<leader>mp', '<cmd>MarkdownPreviewToggle<cr>', desc = '[M]arkdown [P]review' },
+    },
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
